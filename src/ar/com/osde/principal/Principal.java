17b02da6e0b57b0ba1a8a4f05c4aa9d03ae9cec4
Martin Vega <martin.vega@osde.com.ar>
2024-05-19 14:18:28 -0300

    Se agrega comentario al mensaje en consola

--- src/ar/com/osde/principal/Principal.java
+++ src/ar/com/osde/principal/Principal.java
@@ -3,6 +3,6 @@ package ar.com.osde.principal;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");//esto imprime el mensaje en consola
 	}
 }
