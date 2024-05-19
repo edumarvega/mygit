9bfe55f4088c51f0e27b4638c3630c1fe12e0ae1
Martin Vega <martin.vega@osde.com.ar>
2024-05-19 14:30:13 -0300

    voy a continuar tomorrow programando

--- src/ar/com/osde/principal/Principal.java
+++ src/ar/com/osde/principal/Principal.java
@@ -4,5 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");//esto imprime el mensaje en consola
+		
+		//aqui seguro voy a poner nuevas lineas de codigo
 	}
 }
