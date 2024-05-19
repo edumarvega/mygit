f1e78d2559abdc5f50516d7ee6fcf00e106926bb
Martin Vega <martin.vega@osde.com.ar>
2024-05-19 14:25:48 -0300

    se agregar una linea en blanco

--- src/ar/com/osde/principal/Principal.java
+++ src/ar/com/osde/principal/Principal.java
@@ -3,6 +3,7 @@ package ar.com.osde.principal;
 public class Principal {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Hola mundo");//esto imprime el mensaje en consola
 	}
 }
